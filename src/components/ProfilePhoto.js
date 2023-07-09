66b48c61c263cffec5bc6dae38693f8eae90c30d
Yannick.Mathieu <yannick.mathieu@toolib.fr>
2023-07-09 21:20:56 +0200

    construction page profile en 4 block en colonne sur toute la largeur d'écran

--- src/components/ProfilePhoto.js
+++ src/components/ProfilePhoto.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import "../App.css";
-import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineMail, AiFillCamera } from "react-icons/ai";
+// import {GrMail} from "react-icons/gr";  
 import Button from "@mui/joy/Button/Button";
 
 const ProfilePhoto = ({ src }) => {
   return (
     <div className="photoProfileContainer">
+      
+      <div className='leftbox'>
+
+<AiFillCamera size={30}  style={{border: "solid 1px black", borderRadius: "50px", padding: "10px", margin: "0 5px 0 5px", cursor:"pointer"}} />
       <img src={src} alt="profilImage" className="profilePic"/>
-      <AiOutlineMail size={30} style={{border: "solid 1px black", borderRadius: "50px", padding: "10px"}}/>
-      <Button sx={{backgroundColor: "black", borderRadius:"20px", height:"40px"}}>Follow</Button>
+
+      </div>
+      <div className='rightbox'>
+
+      <AiOutlineMail size={30}  className="mailButton"  />
+      <Button 
+      sx={{backgroundColor: "black", borderRadius:"20px", height:"40px", width: "150px"}}
+     
+      // onClick={openMedia}
+      >Follow</Button>
+
+      </div>
     </div>
   );
 }
