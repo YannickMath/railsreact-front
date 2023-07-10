e1f9e8b98af318781762e1a519b92f8fe81ad267
Yannick.Mathieu <yannick.mathieu@toolib.fr>
2023-07-10 08:53:43 +0200

    maj page profile et ses composants

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,36 +1,27 @@
-import React from 'react';
-import ProfilePhoto from './ProfilePhoto';
-import ProfileDescription from './ProfileDescription';
-import ProfileNavigation from './ProfileNavigation';
-import ProfileSearchBar from './ProfileSearchBar.js';
-import "../App.css"
+import React from "react";
+import ProfilePhoto from "./ProfilePhoto";
+import ProfileDescription from "./ProfileDescription";
+import ProfileNavigation from "./ProfileNavigation";
+import ProfileSearchBar from "./ProfileSearchBar.js";
+import "../App.css";
 
 const Profile = () => {
   return (
     <div className="mainProfile">
       <div id="ProfilePhoto">
-      <ProfilePhoto src="/twitter.webp" />
-
+        <ProfilePhoto src="/twitter.webp" />
       </div>
-      <div  id="ProfileDescription">
-      <ProfileDescription />
-        
-        </div>
-        <div id="ProfileNavigation">
-        
-      <ProfileNavigation />
-        </div>
-        <div id="ProfileSearchBar">
+      <div id="ProfileDescription">
+        <ProfileDescription />
+      </div>
+      <div id="ProfileSearchBar">
         <ProfileSearchBar />
-        
-        </div>
- 
+      </div>
+      <div id="ProfileNavigation">
+        <ProfileNavigation />
+      </div>
     </div>
   );
-}
+};
 
 export default Profile;
-
-
-
-
--- src/components/ProfileSearchBar.js
+++ src/components/ProfileSearchBar.js
@@ -1,10 +1,15 @@
 import "../App.css";
+import { AiFillBell, AiOutlineSearch, AiOutlineMail, AiFillHome } from "react-icons/ai";
 
 export default function ProfileSearchBar() {
 
     return (
         <div className="mainContainerSearchBar">
-            <p style={{color:"white"}}>ProfileSearchBar</p>
+            <AiFillBell size={30} className="searchBarButton"/>
+            <AiOutlineSearch size={30} className="searchBarButton"/>
+            <AiOutlineMail size={30} className="searchBarButton"/>
+            <AiFillHome size={30} className="searchBarButton"/>
+            
         </div>
         )
         }
